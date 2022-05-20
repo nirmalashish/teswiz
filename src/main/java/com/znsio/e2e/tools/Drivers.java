dcd0be9078a7a82f2a2e9b09809aeeb9051d8e2c
ashish.nirmal <ashish.nirmal@ril.com>
2022-05-20 14:03:21 +0530

    commented out code for connectivity check to base url

--- src/main/java/com/znsio/e2e/tools/Drivers.java
+++ src/main/java/com/znsio/e2e/tools/Drivers.java
@@ -207,7 +207,7 @@ public class Drivers {
         if (numberOfWebDriversUsed == 0) {
             browserConfig = getBrowserConfig();
             context.addTestState(TEST_CONTEXT.BROWSER_CONFIG, browserConfig);
-            checkConnectivityToBaseUrl(baseUrl);
+            //checkConnectivityToBaseUrl(baseUrl);
         } else {
             browserConfig = (JSONObject) context.getTestState(TEST_CONTEXT.BROWSER_CONFIG);
         }
